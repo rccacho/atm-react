06a97ddf64751773df0abcf6c72ea37c5c456171
rccacho <rccacho@gmail.com>
2017-06-02 19:08:46 -0700

    add ref to text input and click handlers to deposit and withdraw

--- src/Account.js
+++ src/Account.js
@@ -12,9 +12,9 @@ export default class Account extends Component {
       <div className="account">
         <h2>{this.props.name}</h2>
         <div className="balance">$0</div>
-        <input type="text" placeholder="enter an amount" />
-        <input type="button" value="Deposit" />
-        <input type="button" value="Withdraw" />
+        <input type="text" placeholder="enter an amount" ref="amount" />
+        <input type="button" value="Deposit" onClick={(e) => this.handleDepositClick(e)} />
+        <input type="button" value="Withdraw" onClick={(e) => this.handleWithdrawClick(e)} />
       </div>
     )
   }
